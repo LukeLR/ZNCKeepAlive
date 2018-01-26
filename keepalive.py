aabbd101e246a05417ff5b111b546b261e3f22b2
Lukas <public@lrose.de>
2018-01-26 17:39:18 +0100

    substituted ping for version

--- keepalive.py
+++ keepalive.py
@@ -2,14 +2,14 @@ import znc
 
 class pingtimer(znc.Timer):
     def RunJob(self):
-        self.GetModule().PutModule('Sending /ping number {0} to IRC network...'.format(self.counter))
-        self.GetModule().PutIRC('/ping')
+        self.GetModule().PutModule('Sending version number {0} to IRC network...'.format(self.counter))
+        self.GetModule().PutIRC('version')
         self.counter = self.counter + 1
 
 class keepalive(znc.Module):
     description = "Keep connections through NAT to idle IRC networks alive."
     module_types = [znc.CModInfo.NetworkModule,znc.CModInfo.GlobalModule,znc.CModInfo.UserModule]
     def OnIRCConnected(self):
-        timer = self.CreateTimer(pingtimer, interval=4, cycles=0, description='Sends ping command to IRC network.')
+        timer = self.CreateTimer(pingtimer, interval=4, cycles=0, description='Sends version command to IRC network.')
         timer.counter=0
         return znc.CONTINUE
