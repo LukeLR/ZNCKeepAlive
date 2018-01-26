8999b78d09f0c9f640fdfde20527cffe0dd47302
Lukas <public@lrose.de>
2018-01-26 17:05:32 +0100

    further testing

--- keepalive.py
+++ keepalive.py
@@ -8,9 +8,11 @@ class keepalive(znc.Module):
     description = "Keep connections through NAT to idle IRC networks alive."
     module_types = [znc.CModInfo.GlobalModule,znc.CModInfo.UserModule,znc.CModInfo.NetworkModule]
     def OnIRCConnected(self):
-        timer = self.CreateTimer(testtimer, interval=4, cycles=1, description='Says "foo bar" after 4 seconds')
+        timer = self.CreateTimer(pingtimer, interval=4, cycles=2, description='Says "foo bar" twice after 4 seconds')
         timer.msg = 'bar'
         return znc.CONTINUE
     def OnChanMsg(self, nick, channel, message):
         self.PutModule("Hey, {0} said {1} on {2}".format(nick.GetNick(), message.s, channel.GetName()))
+         timer = self.CreateTimer(pingtimer, interval=4, cycles=2, description='Says "foo bar" twice after 4 seconds')
+        timer.msg = 'chan'
         return znc.CONTINUE
